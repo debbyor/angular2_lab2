e8505e10511d15e002e74ae6ef1cf3d0abaa15d4
Enda Lee <enda.lee@ittdublin.ie>
2016-11-16 13:33:50 +0000

    Starting lab 2

--- src/app/show-message/show-message.component.ts
+++ src/app/show-message/show-message.component.ts
@@ -6,14 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-message.component.css']
 })
 export class ShowMessageComponent implements OnInit {
+  //Declare and in itialise a string
   clickMessage: string ="";
 
   constructor() {}
 
+  //This methos handles the button click
+  //Sets the clickMessage property = input
+  //No validation - bad idea!
   onClickMe(myMessage: string){
   this.clickMessage = myMessage;
   }
-  
+
   ngOnInit() {
   }
 
